1cc7aecf32e3b0eaae6497f042be469e0d405c28
Ashiqur Rahman <ashikrobi@gmail.com>
2021-01-21 22:31:17 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,21 @@
+// https://github.com/ashikrobi/basic-js
+
+//project kilometerToMeter
+function kilometerToMeter(){
+
+}
+
+//project budgetCalculator
+function budgetCatculator(){
+
+}
+
+//project hotelCost
+function hotelCost(){
+
+}
+
+//project megaFriend
+function megaFriend(){
+    
+}
